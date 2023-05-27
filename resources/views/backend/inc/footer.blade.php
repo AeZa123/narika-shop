06bd098b601d03c4444d74106d7b731de3536fb6
Ae <aymexe1212@gmail.com>
2023-05-27 22:07:15 +0700

    install layout dashboard adminLTE

---
+++ resources/views/backend/inc/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('assets/templateLTE/plugins/jquery/jquery.min.js')}}"></script>
+{{-- <script src="plugins/jquery/jquery.min.js"></script> --}}
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('assets/templateLTE/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('assets/templateLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- ChartJS -->
+{{-- <script src="{{asset('assets/templateLTE/plugins/chart.js/Chart.min.js')}}"></script> --}}
+<!-- Sparkline -->
+{{-- <script src="{{asset('assets/templateLTE/plugins/sparklines/sparkline.js')}}"></script> --}}
+<!-- JQVMap -->
+{{-- <script src="{{asset('assets/templateLTE/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('assets/templateLTE/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script> --}}
+<!-- jQuery Knob Chart -->
+{{-- <script src="{{asset('assets/templateLTE/plugins/jquery-knob/jquery.knob.min.js')}}"></script> --}}
+<!-- daterangepicker -->
+{{-- <script src="{{asset('assets/templateLTE/plugins/moment/moment.min.js')}}"></script> --}}
+{{-- <script src="{{asset('assets/templateLTE/plugins/daterangepicker/daterangepicker.js')}}"></script> --}}
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{asset('assets/templateLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<!-- Summernote -->
+{{-- <script src="{{asset('assets/templateLTE/plugins/summernote/summernote-bs4.min.js')}}"></script> --}}
+<!-- overlayScrollbars -->
+<script src="{{asset('assets/templateLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('assets/templateLTE/dist/js/adminlte.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+{{-- <script src="dist/js/demo.js"></script> --}}
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('assets/templateLTE/dist/js/pages/dashboard.js')}}"></script>
---
+++ resources/views/backend/views/m_users/list.blade.php
@@ -0,0 +1,15 @@
+@extends('backend.layouts.dashboard_layout')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <h1>List User</h1>
+
+    </div>
+</div>
+
+
+
+    
+@endsection
